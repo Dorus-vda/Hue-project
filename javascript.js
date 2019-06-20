408a7e8f1b8efb6e4cbbfbafc3fae070e5f9e2ff
Doruzzz <44002080+Doruzzz@users.noreply.github.com>
2019-06-20 12:40:13 +0200

    Alle files

---
+++ javascript.js
@@ -0,0 +1,53 @@
+const BRIDGE_URL = "192.168.178.172"  // IP adres van de bridge
+const API_KEY = "l1SJ36Y-mE6pM48fRULsOjfFIv2tyV68AWtcXNjB"  //sleutel om de bridge aan te sturen
+const BASE_URL = `http://${BRIDGE_URL}/api/${API_KEY}/lights/` //beginstuk van de url
+
+navigator.getBattery().then(function(battery) {
+    console.log("test: " + battery.level);
+});
+
+function zetAan(){
+  let body = '{"on": true}';
+  sendRequest(1, body);
+}
+
+function zetUit(){
+  let body = '{"on": false}';
+  sendRequest(1, body);
+}
+
+function Rood(){
+  let body = '{"hue": 255}';
+  sendRequest(1, body);
+}
+
+function Groen(){
+  let body = '{"hue": 14000}';
+  sendRequest(1, body);
+}
+
+function Bri(){
+  let body = '{"bri": "150"}';
+  sendRequest(1, body);
+}
+
+function sendRequest(lampNumber, body){
+	let http = new XMLHttpRequest();
+       let url = BASE_URL + lampNumber + "/state";
+	http.open("PUT", url);  //We gebruiken de URL om een PUT request naartoe te sturen
+	http.onreadystatechange = function() {
+		if(http.readyState == 4 && http.status == 200){
+			console.log(http.responseText);
+		}
+	}
+	http.send(body);  //Stuur de body van je request naar de bridge
+}
+
+var aanknop = document.getElementById("aanknop");
+var uitknop = document.getElementById("uitknop");
+var rood = document.getElementById("rood");
+var groen = document.getElementById("groen");
+aanknop.addEventListener("click", zetAan);
+uitknop.addEventListener("click", zetUit);
+rood.addEventListener("click", Rood);
+groen.addEventListener("click", Groen);
